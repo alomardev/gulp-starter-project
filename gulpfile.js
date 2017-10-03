a78dc233a40d68d58dd79d325f8c69bfc247f295
Abdulrahman Alomar <alomardev@gmail.com>
2017-10-03 08:07:26 +0300

    php support

--- gulpfile.js
+++ gulpfile.js
@@ -8,98 +8,118 @@ autoprefixer = require('gulp-autoprefixer'),
      htmlmin = require('gulp-htmlmin'),
      cssnano = require('gulp-cssnano'),
       uglify = require('gulp-uglify'),
-     connect = require('gulp-connect'),
-    delempty = require('delete-empty'),
-         del = require('del');
+     connect = require('gulp-connect-php'),
 
-var dir = {
-	app: 'app',
-	prod: 'build/production',
-	dev: 'build/development'
+    delempty = require('delete-empty'),
+         del = require('del'),
+        exec = require('child_process').exec,
+
+       iswin = /^win/.test(process.platform),
+       ismac = /^darwin/.test(process.platform);
+
+var config = {
+	port: 8080,
+	dir: {
+		app: 'app',
+		prod: 'build/production',
+		dev: 'build/development'
+	},
+	php: {
+		bin: iswin ? 'C:\\Users\\coop\\backend\\php\\php.exe' : '/usr/bin/php',
+		ini: iswin ? 'C:\\Users\\coop\\backend\\php\\php.ini' : '/etc/php.ini'
+	}
 };
+
+function runCommand(command, input) {
+  ls = exec(command, function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    if (err !== null) console.log(err);
+  });
+
+  ls.stdin.write(input);
+}
 
 /* Scripts */
 gulp.task('scripts', ['clean:scripts'], function() {
-	return gulp.src(dir.app + '/res/scripts/*.js')
+	return gulp.src(config.dir.app + '/res/scripts/*.js')
 	.pipe(plumber())
-	.pipe(gulp.dest(dir.dev + '/res'))
-	.pipe(connect.reload());
+	.pipe(gulp.dest(config.dir.dev + '/res'));
 });
 
 /* Styles */
 gulp.task('styles', ['clean:styles'], function() {
-	return gulp.src(dir.app + '/res/styles/main.{scss,sass}')
+	return gulp.src(config.dir.app + '/res/styles/main.{scss,sass}')
 	.pipe(plumber())
 	.pipe(sass())
-	.pipe(autoprefixer())
-	.pipe(gulp.dest(dir.dev + '/res'))
-	.pipe(connect.reload());
+	.pipe(autoprefixer('last 10 versions'))
+	.pipe(gulp.dest(config.dir.dev + '/res'));
 });
 
-/* HTMLs */
-gulp.task('htmls', ['clean:htmls'], function() {
-	return gulp.src([dir.app + '/**/*.html', '!' + dir.app + '/**/_*.html'])
+/* Markup */
+gulp.task('markup', ['clean:markup'], function() {
+	return gulp.src([config.dir.app + '/**/*.{html,php}', '!' + config.dir.app + '/**/_*.{html,php}'])
 	.pipe(plumber())
-	.pipe(fileinclude(/* Default: {prefix: '@@', path: '@file'} */))
-	.pipe(gulp.dest(dir.dev))
-	.pipe(connect.reload());
+	.pipe(fileinclude())
+	.pipe(gulp.dest(config.dir.dev));
 });
 
 /* Copy images */
 gulp.task('copy:images', ['clean:images'], function() {
-	return gulp.src(dir.app + '/res/images/*.{png,svg,gif,jpg,jpeg}')
-	.pipe(gulp.dest(dir.dev + '/res/'));
+	return gulp.src(config.dir.app + '/res/images/*.{png,gif,jpg,jpeg,svg,ico}')
+	.pipe(gulp.dest(config.dir.dev + '/res/'));
 });
 
 /* Copy raw files */
 gulp.task('copy:raw', ['clean:raw'], function() {
-	return gulp.src(dir.app + '/raw/**/*')
-	.pipe(gulp.dest(dir.dev + '/raw'));
+	return gulp.src(config.dir.app + '/raw/**/*')
+	.pipe(gulp.dest(config.dir.dev + '/raw'));
 });
 
-/* Server setup */
-gulp.task('connect', function() {
-	connect.server({
-		root: './' + dir.dev + '/',
-		livereload: true
-	});
+gulp.task('copy:plugins', ['clean:plugins'], function() {
+	return gulp.src(config.dir.app + '/plugins/**/*')
+	.pipe(gulp.dest(config.dir.dev + '/plugins'));
 });
 
 /* Clean */
 gulp.task('clean:scripts', function() {
-	del.sync(dir.dev + '/res/*.js');
+	del.sync(config.dir.dev + '/res/*.js');
 });
 
 gulp.task('clean:styles', function() {
-	del.sync(dir.dev + '/res/main.min.css');
+	del.sync(config.dir.dev + '/res/main.css');
 });
 
-gulp.task('clean:htmls', ['clean:empty'], function() {
-	del.sync(dir.dev + '/**/*.html');
+gulp.task('clean:markup', ['clean:empty'], function() {
+	del.sync(config.dir.dev + '/**/*.{html,php}');
 });
 
 gulp.task('clean:images', function() {
-	del.sync(dir.dev + '/res/images/*.{png,svg,gif,jpg,jpeg}');
+	del.sync(config.dir.dev + '/res/images/*.{png,gif,jpg,jpeg,svg,ico}');
 });
 
 gulp.task('clean:raw', function() {
-	del.sync(dir.dev + '/raw');
+	del.sync(config.dir.dev + '/raw');
+});
+
+gulp.task('clean:plugins', function() {
+	del.sync(config.dir.dev + '/plugins');
 });
 
 gulp.task('clean:empty', function() {
-	delempty.sync(dir.dev + '/');
+	delempty.sync(config.dir.dev + '/');
 });
 
 gulp.task('clean', function() {
-	del.sync(dir.dev);
-	del.sync(dir.prod);
+	del.sync(config.dir.dev);
+	del.sync(config.dir.prod);
 });
 
 /* Build */
-gulp.task('build', ['scripts', 'styles', 'htmls', 'copy:images', 'copy:raw']);
+gulp.task('build', ['scripts', 'styles', 'markup', 'copy:images', 'copy:raw', 'copy:plugins']);
 gulp.task('build:dev', ['build']);
 gulp.task('build:prod', ['build'], function() {
-	del.sync(dir.prod);
+	del.sync(config.dir.prod);
 
 	var condjs = function(file) {
 		return gulpmatch(file, '*.js');
@@ -111,23 +131,41 @@ gulp.task('build:prod', ['build'], function() {
 		return gulpmatch(file, '*.html');
 	};
 
-	gulp.src(dir.dev + '/**/*')
+	gulp.src(config.dir.dev + '/**/*')
 	.pipe(plumber())
 	.pipe(gulpif(condcss, cssnano()))
 	.pipe(gulpif(condjs, uglify()))
-	.pipe(gulpif(condhtml, htmlmin({collapseWhitespace: true})))
-	.pipe(gulp.dest(dir.prod));
+	//.pipe(gulpif(condhtml, htmlmin({collapseWhitespace: true})))
+	.pipe(gulp.dest(config.dir.prod));
+	gulp.src(config.dir.app + '/**/.htaccess').pipe(gulp.dest(config.dir.prod));
 
 });
 
 /* Watch */
 gulp.task('watch', ['build'], function() {
-	gulp.watch(dir.app + '/res/scripts/*.js', ['scripts']);
-	gulp.watch(dir.app + '/res/styles/**/*.{scss,sass}', ['styles']);
-	gulp.watch(dir.app + '/**/*.html', ['htmls']);
-	gulp.watch(dir.app + '/res/images/*.{png,svg,gif,jpg,jpeg}', ['copy:images']);
-	gulp.watch(dir.app + '/raw/**/*', ['copy:raw']);
+	gulp.watch(config.dir.app + '/res/scripts/*.js', ['scripts']);
+	gulp.watch(config.dir.app + '/res/styles/*.{scss,sass}', ['styles']);
+	gulp.watch(config.dir.app + '/**/*.{html,php}', ['markup']);
+	gulp.watch(config.dir.app + '/res/images/*.{png,gif,jpg,jpeg,svg,ico}', ['copy:images']);
+	
+	gulp.watch(config.dir.app + '/raw/**/*', ['copy:raw']);
+	gulp.watch(config.dir.app + '/plugins/**/*', ['copy:plugins']);
+});
+
+/* Server */
+var server = new connect();
+gulp.task('server:start', function() {
+	server.server({
+		base: config.dir.dev,
+		bin: config.php.bin,
+		ini: config.php.ini,
+		port: config.port
+	});
+});
+
+gulp.task('server:stop', function() {
+	server.closeServer();
 });
 
 /* Default */
-gulp.task('default', ['watch', 'connect']);
+gulp.task('default', ['watch', 'server:start']);
