428bc682bfac3139dc6f56fd5f07f92743e39278
Abdulrahman Alomar <alomardev@gmail.com>
2017-10-08 13:09:12 +0300

    fixing bootstrap settings

--- app/_bottom.php
+++ app/_bottom.php
@@ -1,5 +1,6 @@
-<script src='/res/bootstrap.js'></script>
 <script src='/res/jquery.js'></script>
+<script src='/res/popper.js'></script>
+<script src='/res/bootstrap.js'></script>
 <script src='/res/main.js'></script>
 @@for (var i = 0; i < scripts.length; i++) {<script src='`+scripts[i]+`'></script>}
 </body>
